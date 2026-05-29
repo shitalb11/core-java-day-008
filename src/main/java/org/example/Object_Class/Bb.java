0e8de3f513c3b6e2c3328861c784ea9da88117e3
Shital Bhosale <shitalb075@gmail.com>
2026-05-29 21:15:54 +0530

    newwwwww

--- src/main/java/org/example/Object_Class/Bb.java
+++ src/main/java/org/example/Object_Class/Bb.java
@@ -2,6 +2,12 @@ package org.example.Object_Class;
 
 public class Bb {
     public static void main(String[] args) {
+     class A{
+    public static void m1(){
+        System.out.println("A::m1");
+    }
+     }
+
 
     }
 }
