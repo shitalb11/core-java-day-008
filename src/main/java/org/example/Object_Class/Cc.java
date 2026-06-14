cb8d2f99e360b48b9cd54d6877b1356b1e69d13c
Shital Bhosale <shitalb075@gmail.com>
2026-06-14 13:59:13 +0530

    new

--- src/main/java/org/example/Object_Class/Cc.java
+++ src/main/java/org/example/Object_Class/Cc.java
@@ -1,4 +1,10 @@
 package org.example.Object_Class;
 
 public class Cc {
+    public static void main(String[] args) {
+        Cc obj = new Cc();
+        System.out.println(obj.getClass().getName());
+
+
+    }
 }
