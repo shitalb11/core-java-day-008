194c07c68dfe6d722f3aef763dccab688d3a6282
Shital Bhosale <shitalb075@gmail.com>
2026-06-14 14:20:42 +0530

    new

---
+++ src/main/java/org/example/Object_Class_Practice/_1_Basic_Example_Object_Creation.java
@@ -0,0 +1,12 @@
+package org.example.Object_Class_Practice;
+
+class Student{
+}
+
+public class _1_Basic_Example_Object_Creation {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        System.out.println(s);
+    }
+}
