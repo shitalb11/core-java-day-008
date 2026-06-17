f0aa04ffc40a03af3e06146681dd626eaa8df760
Shital Bhosale <shitalb075@gmail.com>
2026-06-17 21:10:43 +0530

    new

---
+++ src/main/java/org/example/Object_Class_Practice/_5_Student.java
@@ -0,0 +1,13 @@
+package org.example.Object_Class_Practice;
+
+public class _5_Student {
+
+    int id = 101;
+    String name = "Shital";
+
+    public static void main(String[] args) {
+        _5_Student s = new _5_Student();
+        System.out.println(s.id);
+        System.out.println(s.name);
+    }
+}
