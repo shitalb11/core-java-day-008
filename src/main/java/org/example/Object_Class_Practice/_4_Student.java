cc933ca3fbc2f106921f67d941e10cc11549f0f1
Shital Bhosale <shitalb075@gmail.com>
2026-06-17 21:08:17 +0530

    new

---
+++ src/main/java/org/example/Object_Class_Practice/_4_Student.java
@@ -0,0 +1,8 @@
+package org.example.Object_Class_Practice;
+
+public class _4_Student {
+    public static void main(String[] args) {
+        _4_Student s = new _4_Student();
+        System.out.println(s);
+    }
+}
