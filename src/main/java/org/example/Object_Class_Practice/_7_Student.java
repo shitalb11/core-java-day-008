9c548be782a8dad8e1877f2d1732fbad6eeaadd7
Shital Bhosale <shitalb075@gmail.com>
2026-06-19 22:00:33 +0530

    new

---
+++ src/main/java/org/example/Object_Class_Practice/_7_Student.java
@@ -0,0 +1,12 @@
+package org.example.Object_Class_Practice;
+
+public class _7_Student {
+    public static void main(String[] args) {
+
+        _7_Student s1 = new _7_Student();
+        _7_Student s2 = new _7_Student();
+
+        System.out.println(s1.equals(s2));
+    }
+}
+//Equals()
