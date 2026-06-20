2683b7bd239fdbd06d0b21fd39d6e904858d2fc8
Shital Bhosale <shitalb075@gmail.com>
2026-06-20 18:47:57 +0530

    new

--- src/main/java/org/example/Object_Class_Practice/_8_Student.java
+++ src/main/java/org/example/Object_Class_Practice/_8_Student.java
@@ -1,5 +1,16 @@
 package org.example.Object_Class_Practice;
 
 public class _8_Student {
+    int id;
+    _8_Student(){
+        id=101;
+    }
+
+    public boolean equals (Object obj){
+
+        _8_Student s = (_8_Student) obj;
+
+        return this.id == s.id;
+    }
 
 }
