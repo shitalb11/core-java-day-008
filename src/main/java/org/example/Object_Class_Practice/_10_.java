f775b6b8346419324eb892a3ead9b05f4d6e08c1
Shital Bhosale <shitalb075@gmail.com>
2026-06-22 22:43:25 +0530

    new

--- src/main/java/org/example/Object_Class_Practice/_10_.java
+++ src/main/java/org/example/Object_Class_Practice/_10_.java
@@ -1,4 +1,15 @@
 package org.example.Object_Class_Practice;
 
 public class _10_ {
+    int id = 101;
+    public int hashCode(){
+        return id;
+    }
+
+    public static void main(String[] args) {
+        _10_ s = new _10_();
+
+        System.out.println(s.hashCode());
+    }
 }
+//hashCode()
