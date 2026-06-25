78a4b4f36e4ad04681cdaf660022cfe2322a8e5c
Shital Bhosale <shitalb075@gmail.com>
2026-06-25 17:17:30 +0530

    new

--- src/main/java/org/example/Object_Class_Practice/_13_.java
+++ src/main/java/org/example/Object_Class_Practice/_13_.java
@@ -4,6 +4,8 @@ public class _13_ {
     public static void main(String[] args) {
         _13_ s = new _13_();
 
-
+        System.out.println(
+                s.getClass().getName()
+        );
     }
 }
