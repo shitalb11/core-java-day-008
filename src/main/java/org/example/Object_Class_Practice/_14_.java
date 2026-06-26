48dc0e80f4822535c0d5ab7e8691244e5965a47e
Shital Bhosale <shitalb075@gmail.com>
2026-06-26 11:10:00 +0530

    new

--- src/main/java/org/example/Object_Class_Practice/_14_.java
+++ src/main/java/org/example/Object_Class_Practice/_14_.java
@@ -1,5 +1,16 @@
 package org.example.Object_Class_Practice;
 
 public class _14_ {
+    public static void main(String[] args) {
+        _14_ s = new _14_();
 
+        System.out.println(
+                s instanceof _14_
+        );
+
+        System.out.println(
+                s instanceof Object
+        );
+    }
 }
+//instanceof
