877b100d63f989ee9bbb02930f51a637f86f6fa7
Shital Bhosale <shitalb075@gmail.com>
2026-06-28 18:27:11 +0530

    new

--- src/main/java/org/example/Object_Class_Practice/_16_.java
+++ src/main/java/org/example/Object_Class_Practice/_16_.java
@@ -1,4 +1,14 @@
 package org.example.Object_Class_Practice;
 
 public class _16_ {
+
+    void display(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        Object obj = new _16_();
+        System.out.println(obj);
+    }
+
 }
+//upcasting
