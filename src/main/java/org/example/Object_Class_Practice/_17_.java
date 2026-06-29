0ea377d0092e4fc1ffd125e759ab088c8f46300a
Shital Bhosale <shitalb075@gmail.com>
2026-06-29 16:44:27 +0530

    new

--- src/main/java/org/example/Object_Class_Practice/_17_.java
+++ src/main/java/org/example/Object_Class_Practice/_17_.java
@@ -1,5 +1,10 @@
 package org.example.Object_Class_Practice;
 
-public class _17_ {
-
+public class _17_ implements Cloneable {
+    int id = 101;
+    public Object clone()
+        throws
+            CloneNotSupportedException{
+        return super.clone();
+    }
 }
