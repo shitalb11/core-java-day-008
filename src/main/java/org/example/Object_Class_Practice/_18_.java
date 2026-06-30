61c4e7b4cae898aeb486acfd1b34c63108df6d31
Shital Bhosale <shitalb075@gmail.com>
2026-06-30 11:25:48 +0530

    new

--- src/main/java/org/example/Object_Class_Practice/_18_.java
+++ src/main/java/org/example/Object_Class_Practice/_18_.java
@@ -2,7 +2,8 @@ package org.example.Object_Class_Practice;
 
 public class _18_ {
     public static void main(String[] args) {
-
+        _18_ s = new _18_();
+        System.out.println(s.getClass().getSuperclass());
     }
 }
 //Super class check
